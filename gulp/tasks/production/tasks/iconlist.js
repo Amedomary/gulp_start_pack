f537d68592587f0f43efce7a0ef7c133961a7a5b
Maxim Grishin <@Amedomary>
2019-07-01 17:47:48 +0400

    fix icon list

--- gulp/tasks/production/tasks/iconlist.js
+++ gulp/tasks/production/tasks/iconlist.js
@@ -6,8 +6,6 @@ const config  = require('../../../config');
 const Promise = require('bluebird');
 const fs      = require("fs");
 
-var PLUGIN_NAME = 'kontora-iconlist';
-
 /*
  * Build index.html
  */
@@ -15,7 +13,7 @@ module.exports = function(options) {
     return config.wrapPipe(function(success, error) {
         var build_path     = __dirname + "/../../../../src/images/svg_for_icon/";
         var files_list     = [];
-        var index_template = __dirname + '/../../../template/iconlist.pug';
+        var index_template = __dirname + '/../../../template/_iconlist.pug';
 
         var promises = fs.readdirSync(build_path).map(function (file) {
             fs.stat(build_path + file, function (err, stats) {
@@ -23,7 +21,7 @@ module.exports = function(options) {
                     return console.error(err);
                 }
 
-                if (stats.isFile() && file != 'iconlist.pug') {
+                if (stats.isFile() && file != '_iconlist.pug') {
                     files_list.push(file.replace('.svg', ''));
                 }
             });
